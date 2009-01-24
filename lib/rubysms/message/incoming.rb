2e89fd0ea2baa4ff9eb7d024394166f0ead720e0
Adam Mckaig <adam.mckaig@gmail.com>
2009-01-24 08:47:00 -0500

    tracking responses to incoming messages in Incoming#responses

--- lib/rubysms/message/incoming.rb
+++ lib/rubysms/message/incoming.rb
@@ -4,16 +4,19 @@
 
 module SMS
 	class Incoming < Gsm::Incoming
-		attr_reader :backend
+		attr_reader :backend, :responses
+		attr_writer :text
 		
 		def initialize(backend, *rest)
 			@backend = backend
+			@responses = []
 			super(nil, *rest)
 		end
 		
 		def respond(response_text)
 			og = SMS::Outgoing.new(backend, sender, response_text)
 			og.in_response_to = self
+			@responses.push(og)
 			og.send!
 		end
 	end
